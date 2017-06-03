ed9af485a034dc15e15fc07861afdd665c560e94
PLATEL Kevin <platel.kevin@gmail.com>
2017-06-03 18:33:31 +0200

    Adding securiting on null division

--- core/fizzbuzz.go
+++ core/fizzbuzz.go
@@ -2,18 +2,21 @@ package core
 
 import "fmt"
 
-func FizzBuzz(from, to, mutiple1, mutiple2 uint, s1, s2 string) ([]string, error) {
+func FizzBuzz(from, to, multiple1, multiple2 int, s1, s2 string) ([]string, error) {
 	if from > to {
 		return nil, fmt.Errorf("from can't be greater than to (%d > %d)", from, to)
+	}
+	if multiple1 < 1 || multiple2 < 1 {
+		return nil, fmt.Errorf("multiple can't be inferior to 1")
 	}
 
 	resp := make([]string, to - from + 1)
 	for i := 0; from <= to; from++ {
 		tmp := ""
-		if from % mutiple1 == 0 {
+		if from % multiple1 == 0 {
 			tmp += s1
 		}
-		if from % mutiple2 == 0 {
+		if from % multiple2 == 0 {
 			tmp += s2
 		}
 		if len(tmp) == 0 {
