fb66069f3cd3db5a3f8d060a90a905cac92f7869
PLATEL Kevin <platel.kevin@gmail.com>
2017-06-03 15:54:52 +0200

    Implement first route (/version)

--- service/service.go
+++ service/service.go
@@ -1,4 +1,10 @@
-package http
+package service
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 // HttpService define basic possible interaction for all HttpService
 type HttpService interface {
@@ -7,13 +13,48 @@ type HttpService interface {
 
 // FizzBuzzService is a HttpService which
 type FizzBuzzService struct {
-
+	httpClient *http.ServeMux
+	version    string
 }
 
 func CreateFizzBuzzService() HttpService {
-	return &FizzBuzzService{}
+	return &FizzBuzzService{
+		version: "0.0.1",
+	}
+}
+
+// routes will setup all the available route
+func (service *FizzBuzzService) routes() http.Handler {
+	service.httpClient = http.NewServeMux()
+
+	service.httpClient.HandleFunc("/fizzbuzz/v1/version", service.VersionRoute)
+
+	return service.httpClient
 }
 
 func (service *FizzBuzzService) Launch() {
+	log.Println("Service http service...")
+	http.ListenAndServe(fmt.Sprintf(":%d", 8080), applyCORS(service.routes()))
+}
 
+func (service *FizzBuzzService) VersionRoute(resp http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		data, err := CreateVersionMessage(service.version).GetBytesResp()
+		if err != nil {
+			resp.WriteHeader(http.StatusInternalServerError)
+			data, err = CreateErrorMessage("serialization error", "Error during json serialization.").GetBytesResp()
+			resp.Write(data)
+		}
+		resp.Write(data)
+	default:
+		data, err := CreateErrorMessage("wrong method", "/version route only accept GET method.").GetBytesResp()
+		if err != nil {
+			resp.WriteHeader(http.StatusInternalServerError)
+			data, err = CreateErrorMessage("serialization error", "Error during json serialization.").GetBytesResp()
+			resp.Write(data)
+		}
+		resp.WriteHeader(http.StatusMethodNotAllowed)
+		resp.Write(data)
+	}
 }
